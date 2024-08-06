b8773c1f04313f1781d3fbd1337d9e9bc2e32040
VictorMaxima <victormaxchidera@gmail.com>
2024-08-06 18:55:14 +0100

    minorfix

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -6,14 +6,14 @@ from models.state import State
 from . import app_views
 
 
-@app_views.route('/states', methods=['GET'])
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
 def get_states():
     """ Retrieves the list of all State objects """
     states = storage.all(State)
     return jsonify([state.to_dict() for state in states.values()])
 
 
-@app_views.route('/states/<state_id>', methods=['GET'])
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def get_state(state_id):
     """ Retrieves a State object """
     state = storage.get(State, state_id)
@@ -22,7 +22,7 @@ def get_state(state_id):
     return jsonify(state.to_dict())
 
 
-@app_views.route('/states/<state_id>', methods=['DELETE'])
+@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
 def delete_state(state_id):
     """ Deletes a State object """
     state = storage.get(State, state_id)
@@ -33,12 +33,12 @@ def delete_state(state_id):
     return jsonify({}), 200
 
 
-@app_views.route('/states', methods=['POST'])
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
 def create_state():
     """ Creates a State object """
     if not request.is_json:
         abort(400, description="Not a JSON")
-    data = request.get_json()
+    return request.get_json()
     if 'name' not in data:
         abort(400, description="Missing name")
     new_state = State(**data)

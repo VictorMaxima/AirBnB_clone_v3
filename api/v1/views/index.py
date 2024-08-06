31c3be7d6f503c2e1b20cf451ff126151cec8944
VictorMaxima <victormaxchidera@gmail.com>
2024-08-06 12:21:41 +0100

    stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,26 @@
 """ module containing the status route """
 from . import app_views
 from flask import jsonify
-
+from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+from models.place import Place
+from models.user import User
 
 @app_views.route("/status")
 def status():
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats")
+def stats():
+    """ handler for the stats view """
+    return jsonify({"amenities": storage.count(Amenity),
+                     "cities": storage.count(City),
+                     "places": storage.count(Place),
+                     "reviews": storage.count(Review),
+                     "states": storage.count(State),
+                     "users": storage.count(User)})
+

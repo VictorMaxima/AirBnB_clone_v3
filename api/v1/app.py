febe87bb6ae335b97fa9f4006b3efd5be5db9dcd
VictorMaxima <victormaxchidera@gmail.com>
2024-08-06 12:11:14 +0100

    tired

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,6 +6,7 @@ import flask
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+from models import storage
 import os
 from flask import jsonify
 
@@ -28,8 +29,8 @@ def teardown_db(exception):
     storage.close()
 
 
-host_name = os.getenv("HBNB_API_HOST") if os.getenv("HBNB_API_HOST") else "0.0.0.0"
-port_name = os.getenv("HBNB_API_PORT") if os.getenv("HBNB_API_PORT") else "5000"
+host_name = os.getenv("HBNB_API_HOST", "0.0.0.0")
+port_name = os.getenv("HBNB_API_PORT", "5000")
 
 
 if __name__ == '__main__':
